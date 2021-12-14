76af80f387f63bbb93867e1a7c1ec78ed7f79fdc
mmirzay <mirzay.mohsen@gmail.com>
2021-12-14 20:58:32 +0330

    added Advent of Code problems of day 7 half2.

--- AdventOfCode/adventofcode2021/day7/Main.java
+++ AdventOfCode/adventofcode2021/day7/Main.java
@@ -18,12 +18,10 @@ public class Main {
 	private static void half1(File file) throws FileNotFoundException {
 		Scanner input = new Scanner(file);
 		ArrayList<Integer> positions = new ArrayList<>(2000);
-		HashMap<Integer, Integer> counters = new HashMap<>(1000);
 		HashMap<Integer, ArrayList<Integer>> fuels = new HashMap<>(1000);
 		for (String s : input.nextLine().split(",")) {
 			Integer num = Integer.parseInt(s);
 			positions.add(num);
-			counters.compute(num, (k, v) -> (v == null) ? 1 : v + 1);
 			fuels.put(num, new ArrayList<>(2000));
 		}
 
@@ -46,7 +44,44 @@ public class Main {
 
 	private static void half2(File file) throws FileNotFoundException {
 		Scanner input = new Scanner(file);
+		ArrayList<Integer> positions = new ArrayList<>(2000);
+		ArrayList<Integer> positions2 = new ArrayList<>(2000);
+		HashMap<Integer, Integer> counters = new HashMap<>(1000);
+		HashMap<Integer, ArrayList<Integer>> fuels = new HashMap<>(1000);
+		int mnumber = 0;
+		for (String s : input.nextLine().split(",")) {
+			Integer num = Integer.parseInt(s);
+			positions.add(num);
+			if (num > mnumber)
+				mnumber = num;
+			counters.compute(num, (k, v) -> (v == null) ? 1 : v + 1);
+			fuels.put(num, new ArrayList<>(2000));
+		}
+		for (int i = 0; i <= mnumber; i++) {
+			positions2.add(i);
+			fuels.put(i, new ArrayList<>(2000));
+		}
+		for (Integer i : positions2)
+			for (Integer j : positions)
+				if (fuels.get(i).size() < positions.size()) {
+					int k = Math.abs(i - j);
+					int s = 0;
+					while (k >= 1) {
+						s += k;
+						k--;
+					}
+					fuels.get(i).add(s);
+				}
+		long maxSum = Long.MAX_VALUE;
 
+		for (Integer i : positions2) {
+			long sum = 0;
+			for (Integer j : fuels.get(i))
+				sum += j;
+			if (sum < maxSum)
+				maxSum = sum;
+		}
+		System.out.println("half1 result: " + maxSum);
 		input.close();
 
 	}
