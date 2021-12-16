1b804ef127e1d7e38c6bc03e23165139a1f210f0
mmirzay <mirzay.mohsen@gmail.com>
2021-12-16 20:53:18 +0330

    added Advent of Code problems of day 9 half2.

--- AdventOfCode/adventofcode2021/day9/Main.java
+++ AdventOfCode/adventofcode2021/day9/Main.java
@@ -52,9 +52,53 @@ public class Main {
 		input.close();
 	}
 
+	static int basinSize = 0;
+
 	private static void half2(File file) throws FileNotFoundException {
 		Scanner input = new Scanner(file);
+		ArrayList<String> lines = new ArrayList<>();
+		while (input.hasNext())
+			lines.add(input.nextLine());
+
+		int row = lines.size();
+		int column = lines.get(0).length();
+		int nums[][] = new int[row][column];
+		for (int i = 0; i < row; i++) {
+			int j = 0;
+			for (char ch : lines.get(i).toCharArray())
+				nums[i][j++] = Integer.parseInt(ch + "");
+		}
+
+		ArrayList<Integer> basins = new ArrayList<>();
+
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < column; j++) {
+				basinSize = 0;
+				int n = nums[i][j];
+				if (n < 9)
+					computeBasinSize(nums, row, column, i, j);
+
+				if (basinSize != 0)
+					basins.add(basinSize);
+
+			}
+		basins.sort((a, b) -> b - a);
+		long result = basins.get(0) * basins.get(1) * basins.get(2);
+		System.out.println("half2 result: " + result);
 		input.close();
 	}
 
+	private static void computeBasinSize(int[][] nums, int row, int column, int i, int j) {
+		basinSize++;
+
+		nums[i][j] = 9;
+		if (j + 1 < column && nums[i][j + 1] < 9)
+			computeBasinSize(nums, row, column, i, j + 1);
+		if (j - 1 >= 0 && nums[i][j - 1] < 9)
+			computeBasinSize(nums, row, column, i, j - 1);
+		if (i + 1 < row && nums[i + 1][j] < 9)
+			computeBasinSize(nums, row, column, i + 1, j);
+		if (i - 1 >= 0 && nums[i - 1][j] < 9)
+			computeBasinSize(nums, row, column, i - 1, j);
+	}
 }
